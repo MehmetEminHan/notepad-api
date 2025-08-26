cba0a53fed6fe1d9e4dd1a8ab104c91eb11d10ed
MehmetEminHan <biyikmehmetemin@gmail.com>
2025-08-25 22:31:00 -0400

    Initial commit

---
+++ src/main/java/model/Note.java
@@ -0,0 +1,25 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String content;
+    private Date creationDate;
+    private Date modificationDate;
+    private String author;
+
+}
---
+++ src/test/java/com/mehmet/notepad_api/NotepadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmet.notepad_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
