a4bc692cbeb0f97352fc9107cd05c75de40d1145
MehmetEminHan <biyikmehmetemin@gmail.com>
2025-08-26 20:15:56 -0400

    Note JPA repository created (save)

---
+++ src/main/java/com/mehmet/notepad_api/repository/NoteRepository.java
@@ -0,0 +1,8 @@
+package com.mehmet.notepad_api.repository;
+
+import com.mehmet.notepad_api.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoteRepository  extends JpaRepository<Note, Integer> {
+    Note save(Note note);
+}
