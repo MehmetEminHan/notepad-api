64b1c7a8e86e0beaabdb8a5b30bc98ef48935e9e
MehmetEminHan <biyikmehmetemin@gmail.com>
2025-09-07 02:05:27 -0400

    Message constant pattern implemented to NoteController to increase performance (to avoid using string pool)

--- src/main/java/com/mehmet/notepad_api/controller/NoteController.java
+++ src/main/java/com/mehmet/notepad_api/controller/NoteController.java
@@ -4,41 +4,45 @@ import com.mehmet.notepad_api.model.NoteEntity;
 import com.mehmet.notepad_api.model.Response;
 import com.mehmet.notepad_api.services.NoteService;
 import com.mehmet.notepad_api.services.ResponseService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/note")
+@AllArgsConstructor
 public class NoteController extends ResponseService {
 
     private final NoteService noteService;
-
-    public NoteController(NoteService noteService) {
-        this.noteService = noteService;
-    }
+    private static final String SUCCESSFUL = "Successful";
+    private static final String NOTE_SAVED = "Note saved!";
+    private static final String NOTE_DELETED = "NoteEntity deleted\\nNoteEntity id: \"";
+    private static final String NOTE_UPDATED = "Note updated!";
+    private static final String ALL_NOTES = "All notes returned!";
+    private static final String ALL_EDITS = "All edits returned!";
 
     @PostMapping("/save")
     public ResponseEntity<Response> saveNote(@RequestBody NoteEntity noteEntity) {
-        return ResponseEntity.ok(createCustomResponse("Successful", "NoteEntity saved", noteService.addNote(noteEntity)));
+        return ResponseEntity.ok(createCustomResponse(SUCCESSFUL, NOTE_SAVED, noteService.addNote(noteEntity)));
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Response> deleteNote(@PathVariable Integer id) {
-        return ResponseEntity.ok(createCustomResponse("Successful", "NoteEntity deleted\\nNoteEntity id: \"" + id, noteService.deleteNote(id)));
+        return ResponseEntity.ok(createCustomResponse(SUCCESSFUL, NOTE_DELETED + id, noteService.deleteNote(id)));
     }
 
     @PutMapping("/edit")
     public ResponseEntity<Response> updateNote(@RequestBody NoteEntity noteEntity) {
-        return ResponseEntity.ok(createCustomResponse("Successful", "NoteEntity updated", noteService.editNote(noteEntity)));
+        return ResponseEntity.ok(createCustomResponse(SUCCESSFUL, NOTE_UPDATED, noteService.editNote(noteEntity)));
     }
 
     @GetMapping("/get-all")
     public ResponseEntity<Response> getAllNotes() {
-        return ResponseEntity.ok(createCustomResponse("Successful", "Notes", noteService.getAllNotes()));
+        return ResponseEntity.ok(createCustomResponse(SUCCESSFUL, ALL_NOTES, noteService.getAllNotes()));
     }
 
     @GetMapping("/get-all-edits")
     public ResponseEntity<Response> getAllEditedNotes() {
-        return ResponseEntity.ok(createCustomResponse("Successful", "Notes", noteService.getAllEdits()));
+        return ResponseEntity.ok(createCustomResponse(SUCCESSFUL, ALL_EDITS, noteService.getAllEdits()));
     }
 }
