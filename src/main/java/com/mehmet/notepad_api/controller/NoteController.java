9f622506873b98878a9e902864f2c010b82ca818
MehmetEminHan <biyikmehmetemin@gmail.com>
2025-09-07 01:50:25 -0400

    Controllers endpoints renamed for increase readability

--- src/main/java/com/mehmet/notepad_api/controller/NoteController.java
+++ src/main/java/com/mehmet/notepad_api/controller/NoteController.java
@@ -1,4 +1,4 @@
-package com.mehmet.notepad_api.rest;
+package com.mehmet.notepad_api.controller;
 
 import com.mehmet.notepad_api.model.NoteEntity;
 import com.mehmet.notepad_api.model.Response;
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("v1")
+@RequestMapping("/v1/note")
 public class NoteController extends ResponseService {
 
     private final NoteService noteService;
@@ -17,27 +17,27 @@ public class NoteController extends ResponseService {
         this.noteService = noteService;
     }
 
-    @PostMapping("/saveNote")
+    @PostMapping("/save")
     public ResponseEntity<Response> saveNote(@RequestBody NoteEntity noteEntity) {
         return ResponseEntity.ok(createCustomResponse("Successful", "NoteEntity saved", noteService.addNote(noteEntity)));
     }
 
-    @DeleteMapping("/deleteNote/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Response> deleteNote(@PathVariable Integer id) {
         return ResponseEntity.ok(createCustomResponse("Successful", "NoteEntity deleted\\nNoteEntity id: \"" + id, noteService.deleteNote(id)));
     }
 
-    @PostMapping("/editNote")
+    @PutMapping("/edit")
     public ResponseEntity<Response> updateNote(@RequestBody NoteEntity noteEntity) {
         return ResponseEntity.ok(createCustomResponse("Successful", "NoteEntity updated", noteService.editNote(noteEntity)));
     }
 
-    @GetMapping("/getAllNotes")
+    @GetMapping("/get-all")
     public ResponseEntity<Response> getAllNotes() {
         return ResponseEntity.ok(createCustomResponse("Successful", "Notes", noteService.getAllNotes()));
     }
 
-    @GetMapping("/getAllEditedNotes")
+    @GetMapping("/get-all-edits")
     public ResponseEntity<Response> getAllEditedNotes() {
         return ResponseEntity.ok(createCustomResponse("Successful", "Notes", noteService.getAllEdits()));
     }
