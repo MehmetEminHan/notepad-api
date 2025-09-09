9ef1aa01690153f6ed94a8ddb9156c28dbad278f
MehmetEminHan <biyikmehmetemin@gmail.com>
2025-09-08 23:34:41 -0400

    NoteRepository findbytitle added

--- src/main/java/com/mehmet/notepad_api/repository/NoteRepository.java
+++ src/main/java/com/mehmet/notepad_api/repository/NoteRepository.java
@@ -3,5 +3,8 @@ package com.mehmet.notepad_api.repository;
 import com.mehmet.notepad_api.model.NoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends JpaRepository<NoteEntity, Integer> {
+    List<NoteEntity> findByTitle(String title);
 }
